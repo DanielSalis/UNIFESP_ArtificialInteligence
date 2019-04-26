e8781741d8f70efdde089e77d748cfe355357e42
Daniel Amorim Vilela de Salis <38293875+DanielSalis@users.noreply.github.com>
2019-04-26 14:37:17 -0300

    C#

---
+++ Trabalho01/HillClimbing/HillClimbing/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HillClimbing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vai dar bom, confia");
+            Console.ReadLine();
+        }
+    }
+}
