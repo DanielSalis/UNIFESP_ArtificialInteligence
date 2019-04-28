f69f86fe22f9a1944de8a40e89fd8f8c0744641c
Daniel Amorim Vilela de Salis <38293875+DanielSalis@users.noreply.github.com>
2019-04-28 03:47:31 -0300

    C#
    Hillclimbing com h1

--- Trabalho01/HillClimbing/HillClimbing/Program.cs
+++ Trabalho01/HillClimbing/HillClimbing/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,8 +11,32 @@ namespace HillClimbing
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Vai dar bom, confia");
-            Console.ReadLine();
+            StreamWriter writer = new StreamWriter("results.txt");
+            for (int k=0; k < 100; k++)
+            {
+                Board Obj = new Board();
+                Obj.GenerateInitialState();
+                Obj.initialHeuristic = Obj.CalculateH1(Obj.initialState);
+                Obj.currentHeuristic = Obj.CalculateH1(Obj.initialState);
+                Obj.finalHeuristic = Obj.CalculateH1(Obj.initialState);
+                Obj.PrintMatrix();
+
+                while (true)
+                {
+                    
+                    Obj.doBestMove();
+                    if (Obj.currentHeuristic >= Obj.finalHeuristic)
+                    {
+                        break;
+                    }
+                    Obj.finalHeuristic = Obj.currentHeuristic;
+                }
+
+                writer.WriteLine(Obj.initialHeuristic + " " + Obj.finalHeuristic);
+                //Console.WriteLine(Obj.initialHeuristic + " " + Obj.finalHeuristic);
+                //Console.ReadLine();
+            }
+            writer.Close();
         }
     }
 }
