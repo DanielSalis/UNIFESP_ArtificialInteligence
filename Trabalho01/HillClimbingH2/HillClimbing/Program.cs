1320870a1721afc72401293b916f93f4484784f5
Daniel Amorim Vilela de Salis <38293875+DanielSalis@users.noreply.github.com>
2019-04-28 12:13:56 -0300

    C#

---
+++ Trabalho01/HillClimbingH2/HillClimbing/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HillClimbing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter writer = new StreamWriter("results.txt");
+            writer.WriteLine("Hi|Hf|MV");
+            for (int k=0; k < 100; k++)
+            {
+                Board Obj = new Board();
+                Obj.GenerateInitialState();
+
+                //h2
+                Obj.initialHeuristic = Obj.CalculateH2(Obj.initialState);
+                Obj.currentHeuristic = Obj.CalculateH2(Obj.initialState);
+                Obj.finalHeuristic = Obj.CalculateH2(Obj.initialState);
+
+                Obj.PrintMatrix();
+
+                while (true)
+                {
+                    
+                    Obj.doBestMove();
+                    Obj.moves++;
+                    if ((Obj.currentHeuristic >= Obj.finalHeuristic))
+                    {
+                        break;
+                    }
+                    Obj.finalHeuristic = Obj.currentHeuristic;
+                }
+
+                writer.WriteLine(Obj.initialHeuristic + " " + Obj.finalHeuristic+" |"+Obj.moves);
+                //Console.WriteLine(Obj.initialHeuristic + " " + Obj.finalHeuristic);
+                //Console.ReadLine();
+            }
+            writer.Close();
+        }
+    }
+}
